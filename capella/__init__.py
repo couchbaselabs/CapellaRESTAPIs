60a252e19eeac03705dc02555dbe65c2a8b8e00d
Daniel Nagy <daniel.nagy@couchbase.com>
2022-07-19 12:49:25 +0100

    Make repo usable as both git submodule and python package

---
+++ capella/__init__.py
@@ -0,0 +1,12 @@
+from .CapellaExceptions import (
+    MissingAccessKeyError,
+    MissingSecretKeyError,
+    MissingBaseURLError,
+    AllowlistRuleError,
+    UserBucketAccessListError,
+    InvalidUuidError,
+    GenericHTTPError,
+    CbcAPIError
+)
+from .CapellaAPIAuth import CapellaAPIAuth
+from .CapellaAPIRequests import CapellaAPIRequests
