f0a6e354937660daf2eee93518cd465f564b1848
Mohsin Ahmed <mohsin.ahmed@couchbase.com>
2023-05-03 15:39:23 +0530

    Pause change

--- capella/serverless/CapellaAPI.py
+++ capella/serverless/CapellaAPI.py
@@ -156,7 +156,8 @@ class CapellaAPI(CommonCapellaAPI):
     def pause_db(self, database_id):
         url = "{}/internal/support/serverless-hibernation/{}/pause" \
             .format(self.internal_url, database_id)
-        resp = self.request(url, "POST")
+        resp = self._urllib_request(url, "POST", params='',
+                                        headers=self.cbc_api_request_headers)
         return resp
 
     def resume_db(self, database_id):
